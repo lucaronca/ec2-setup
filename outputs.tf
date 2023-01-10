c7b6d314608326a1a7b2d10f1bbb8c8216fc1cc5
lucaronca <luca.ronca07gmail.com>
2023-01-10 10:45:21 +0100

    Output security group id and name

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,11 @@ output "instance_public_dns" {
   description = "Public IPv4 DNS address of the EC2 instance"
   value       = aws_instance.app.public_dns
 }
+
+output "security_group_id" {
+  value = aws_security_group.app-sg.id
+}
+
+output "security_group_name" {
+  value = aws_security_group.app-sg.name
+}
